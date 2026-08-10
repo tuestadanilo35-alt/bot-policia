8a0c137d178494ae4042eefee1c439258dbbb956
tuestadanilo35-alt <tuestadanilo35@gmail.com>
2026-08-09 20:28:07 -0500

    index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new Client({
 });
 
 // Coloca tu Token de Discord aquí
-const TOKEN = 'MTUzNTg4OTI2OTA0MDg3MzU4Mw.G4zQrK.cON0tdzmXQ-8LtckLkD_ZedZIv7nI3rgtOHToE';
+const TOKEN = process.env.TOKEN;
 // ID del canal donde llegarán las postulaciones para revisiones
 const CANAL_REVISIONES_ID = '1536124979601604638';
 
